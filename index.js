063da7fb619b3a8f8fa854e255c8355b09f504a5
Madhusudhan Gharti <193937305+msg-zero@users.noreply.github.com>
2025-01-24 17:51:31 +0545

    add algorithm

---
+++ index.js
@@ -0,0 +1,27 @@
+// display instructions
+
+// create variables computerScore and humanScore to store computer and human score
+// initialize computerScore and humanScore to 0
+
+// create variable called roundNumber to record number of rounds
+// initialize roundNumber to 0
+
+// create function called getComputerChoice to get a random choice from computer
+
+// create function called getHumanChoice to get human choice
+
+// create function called playRound with arguments humanChoice and computerChoice to evaluate result
+
+// create function called playGame to call playRound() when roundNumber <=5
+// IF roundNumber <=5 THEN
+    // increment roundNumber
+    // start game
+    // get computer choice
+    // get human choice
+    // evaluate winner
+    // increment score with respect to winner
+    // display result
+// ELSE
+    // display total score and winner
+    // prompt the user to exit or play again
+// ENDIF
